ea80520b62df2b4f64368c199f5536374abd0df5
Richard <richardheyes@gmail.com>
2021-05-09 17:14:06 +0100

    Update RGraph.common.core.js
    Add the two hours constants (12 and 24 hrs)

--- libraries/RGraph.common.core.js
+++ libraries/RGraph.common.core.js
@@ -71,6 +71,8 @@
     RGraph.MONTHS_LONG  = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     RGraph.DAYS_SHORT   = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
     RGraph.DAYS_LONG    = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    RGraph.HOURS24      = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'];
+    RGraph.HOURS12      = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00'];
 
 
 
