2a0bb576432a97737545157c220a4bdfd7d50fad
Richard <richardheyes@gmail.com>
2021-10-10 09:10:02 +0100

    Update RGraph.drawing.background.js
    Change the default tooltips effect to slide

--- libraries/RGraph.drawing.background.js
+++ libraries/RGraph.drawing.background.js
@@ -100,6 +100,7 @@
             tooltipsHighlight:          true,
             tooltipsCssClass:           'RGraph_tooltip',
             tooltipsCss:                null,
+            tooltipsEffect:             'slide',
             tooltipsEvent:              'onclick',
             tooltipsFormattedThousand:  ',',
             tooltipsFormattedPoint:     '.',
