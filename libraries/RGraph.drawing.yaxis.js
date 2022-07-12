772d4875957c0e7e38da692e4cd08395232f2561
Richard <richardheyes@gmail.com>
2022-07-12 22:24:20 +0100

    Update RGraph.drawing.yaxis.js
    Update the textAccessible property to default to false

--- libraries/RGraph.drawing.yaxis.js
+++ libraries/RGraph.drawing.yaxis.js
@@ -88,7 +88,7 @@
             textColor:            'black',
             textBold:             false,
             textItalic:           false,
-            textAccessible:       true,
+            textAccessible:       false,
             textAccessibleOverflow:'visible',
             textAccessiblePointerevents: false,
             text:                 null,
