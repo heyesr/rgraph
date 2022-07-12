70872cdf6ba9f543f77171a3870a728bf9e118f6
Richard <richardheyes@gmail.com>
2022-07-12 22:25:34 +0100

    Update RGraph.vprogress.js
    Update the textAccessible property to default to false

--- libraries/RGraph.vprogress.js
+++ libraries/RGraph.vprogress.js
@@ -87,7 +87,7 @@
             textFont:                       'Arial, Verdana, sans-serif',
             textBold:                       false,
             textItalic:                     false,
-            textAccessible:                 true,
+            textAccessible:                 false,
             textAccessibleOverflow:         'visible',
             textAccessiblePointerevents:    false,
             text:                           null,
