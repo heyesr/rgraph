ea175d2836a3e6478539eebc4c2285b1b5d47a3b
Richard <richardheyes@gmail.com>
2022-07-12 22:25:06 +0100

    Update RGraph.radar.js
    Update the textAccessible property to default to false

--- libraries/RGraph.radar.js
+++ libraries/RGraph.radar.js
@@ -125,7 +125,7 @@
             textColor:            'black',
             textBold:             false,
             textItalic:           false,
-            textAccessible:               true,
+            textAccessible:               false,
             textAccessibleOverflow:      'visible',
             textAccessiblePointerevents: false,
             text:                        null,
