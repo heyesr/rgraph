4689d9e2202c779fc1c4a5c3e297df843f6c9b1b
Richard <richardheyes@gmail.com>
2022-10-25 21:33:46 +0100

    Update RGraph.radar.js
    Added subtitle

--- libraries/RGraph.radar.js
+++ libraries/RGraph.radar.js
@@ -145,6 +145,14 @@
             titleValign:          null,
             titleOffsetx:         0,
             titleOffsety:         0,
+            titleSubtitle:        null,
+            titleSubtitleSize:    null,
+            titleSubtitleColor:   '#aaa',
+            titleSubtitleFont:    null,
+            titleSubtitleBold:    null,
+            titleSubtitleItalic:  null,
+            titleSubtitleOffsetx: 0,
+            titleSubtitleOffsety: 0,
 
             linewidth:             1,
 
