c3413505fafbf8ba6a252ee41450ad9f69af0e71
Richard <richardheyes@gmail.com>
2023-01-15 11:57:27 +0000

    Update RGraph.bar.js
    Change the subtitle Default

--- libraries/RGraph.bar.js
+++ libraries/RGraph.bar.js
@@ -216,7 +216,7 @@
             titleItalic:           null,
             titleOffsetx:          0,
             titleOffsety:          0,
-            titleSubtitle:        null,
+            titleSubtitle:        '',
             titleSubtitleSize:    null,
             titleSubtitleColor:   '#aaa',
             titleSubtitleFont:    null,
