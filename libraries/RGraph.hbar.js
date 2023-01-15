fcbbd5aeed545721f78960007e17b0d0a383a510
Richard <richardheyes@gmail.com>
2023-01-15 12:00:36 +0000

    Update RGraph.hbar.js
    Change the subtitle Default

--- libraries/RGraph.hbar.js
+++ libraries/RGraph.hbar.js
@@ -96,7 +96,7 @@
             titleValign:           null,
             titleOffsetx:          0,
             titleOffsety:          0,
-            titleSubtitle:        null,
+            titleSubtitle:        '',
             titleSubtitleSize:    null,
             titleSubtitleColor:   '#aaa',
             titleSubtitleFont:    null,
