333cdae1ca9d889f4eb347bb67bf51fc2e733d6c
Richard <richardheyes@gmail.com>
2023-01-15 11:57:44 +0000

    Update RGraph.funnel.js
    Change the subtitle Default

--- libraries/RGraph.funnel.js
+++ libraries/RGraph.funnel.js
@@ -84,7 +84,7 @@
             titleValign:           null,
             titleOffsetx:          0,
             titleOffsety:          0,
-            titleSubtitle:        null,
+            titleSubtitle:        '',
             titleSubtitleSize:    null,
             titleSubtitleColor:   '#aaa',
             titleSubtitleFont:    null,
