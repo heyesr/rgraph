6b5df63a4709e5bedaf7451bc5dcef005d99b53b
Richard <richardheyes@gmail.com>
2023-04-22 17:22:50 +0100

    Update demos.js
    Update the function which shows source code with JS

--- demos/demos.js
+++ demos/demos.js
@@ -1,5 +1,5 @@
 //
-// This file is (or ideally, will be) included by all demo files
+// This file is included by all demo files
 //
 // REMEMBER THAT YOU MAY NEED TO EDIT DEMOS-ACTUAL.JS
 //
@@ -17,7 +17,7 @@ function showSource ()
 
     for (var i=0; i<tags.length; ++i) {
         if (tags[i].innerHTML.indexOf('new RGraph.') > 0 && tags[i].innerHTML.indexOf('.innerHTML.indexOf(')<= 0) {
-            var pre = document.createElement('samp');
+            var pre = document.createElement('pre');
                 pre.innerHTML = ('    &lt;script&gt;' + tags[i].innerHTML + '&lt;/script&gt;')
                     .replace(/^    /,'')
                     .replace(/\n    /g,'\n')
@@ -26,11 +26,13 @@ function showSource ()
                     .replace(regexp, '<span>//$1</span>') // Highlight comments
                 pre.style.backgroundColor = '#eee';
                 pre.style.backgroundImage = 'linear-gradient(-45deg, rgba(255,255,255,0.5), transparent)';
-                pre.style.lineHeight = '20px !important';
-                pre.style.overflow = 'auto';
-                pre.style.padding = '15px';
-                pre.style.display = 'block';
-                pre.style.whiteSpace = 'pre';
+                pre.style.lineHeight      = '20px !important';
+                pre.style.overflow        = 'auto';
+                pre.style.padding         = '0px';
+                pre.style.display         = 'block';
+                pre.style.whiteSpace      = 'pre';
+                pre.className             = 'code';
+            
             document.getElementById('dynamic-source-code').insertBefore(
                 pre,
                 null
