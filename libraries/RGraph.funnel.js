94562a9cc5ad087fc5fb8cf956bd417ff3c584c3
Richard <richardheyes@gmail.com>
2023-07-06 17:55:32 +0100

    Update RGraph.funnel.js
    Add support for inline responsive configuration

--- libraries/RGraph.funnel.js
+++ libraries/RGraph.funnel.js
@@ -379,6 +379,32 @@
             //
             RGraph.fireCustomEvent(this, 'ondraw');
 
+
+
+
+
+
+
+
+
+            //
+            // Install any inline responsive configuration. This
+            // should be last in the draw function - even after
+            // the draw events.
+            //
+            RGraph.installInlineResponsive(this);
+
+
+
+
+
+
+
+
+            
+
+
+
             return this;
         };
 
