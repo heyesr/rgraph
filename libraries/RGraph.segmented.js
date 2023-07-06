c21ebfb5819f1cec0636609fbadecb65d7753cab
Richard <richardheyes@gmail.com>
2023-07-06 17:57:00 +0100

    Update RGraph.segmented.js
    Add support for inline responsive configuration

--- libraries/RGraph.segmented.js
+++ libraries/RGraph.segmented.js
@@ -329,6 +329,29 @@
 
 
 
+
+
+
+
+
+
+            //
+            // Install any inline responsive configuration. This
+            // should be last in the draw function - even after
+            // the draw events.
+            //
+            RGraph.installInlineResponsive(this);
+
+
+
+
+
+
+
+
+
+
+
             return this;
         };
 
