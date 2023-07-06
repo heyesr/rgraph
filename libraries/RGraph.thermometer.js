ea0571857ba9682405bbe2bd3aea96304dd318ac
Richard <richardheyes@gmail.com>
2023-07-06 17:59:41 +0100

    Update RGraph.thermometer.js
    Add support for inline responsive configuration

--- libraries/RGraph.thermometer.js
+++ libraries/RGraph.thermometer.js
@@ -437,6 +437,29 @@
             //
             RGraph.fireCustomEvent(this, 'ondraw');
 
+
+
+
+
+
+
+
+
+
+            //
+            // Install any inline responsive configuration. This
+            // should be last in the draw function - even after
+            // the draw events.
+            //
+            RGraph.installInlineResponsive(this);
+
+
+
+
+
+
+
+
             return this;
         };
 
