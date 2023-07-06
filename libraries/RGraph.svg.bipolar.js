fede59eef2c3451f4ec440f4b69d49fbd1785b45
Richard <richardheyes@gmail.com>
2023-07-06 17:57:31 +0100

    Update RGraph.svg.bipolar.js
    Add support for inline responsive configuration

--- libraries/RGraph.svg.bipolar.js
+++ libraries/RGraph.svg.bipolar.js
@@ -582,6 +582,27 @@
             // Fire the draw event
             RGraph.SVG.fireCustomEvent(this, 'ondraw');
 
+
+
+
+
+
+
+            //
+            // Install any inline responsive configuration. This
+            // should be last in the draw function - even after
+            // the draw events.
+            //
+            RGraph.SVG.installInlineResponsive(this);
+
+
+
+
+
+
+
+
+
             return this;
         };
 
