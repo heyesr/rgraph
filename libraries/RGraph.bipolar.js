31198d025b5c81ae84df00db8515514dd4017aca
Richard <richardheyes@gmail.com>
2023-07-06 17:51:18 +0100

    Update RGraph.bipolar.js
    Add support for inline responsive configuration

--- libraries/RGraph.bipolar.js
+++ libraries/RGraph.bipolar.js
@@ -594,6 +594,30 @@
             //
             RGraph.fireCustomEvent(this, 'ondraw');
 
+
+
+
+
+
+
+
+            //
+            // Install any inline responsive configuration. This
+            // should be last in the draw function - even after
+            // the draw events.
+            //
+            RGraph.installInlineResponsive(this);
+
+
+
+
+
+
+
+
+
+
+
             return this;
         };
 
