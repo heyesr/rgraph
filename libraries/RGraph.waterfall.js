776f6ce3a3340719d137ee12e4abee02e7f0edb5
Richard <richardheyes@gmail.com>
2023-07-06 17:59:51 +0100

    Update RGraph.waterfall.js
    Add support for inline responsive configuration

--- libraries/RGraph.waterfall.js
+++ libraries/RGraph.waterfall.js
@@ -676,6 +676,30 @@
             // Fire the RGraph draw event
             //
             RGraph.fireCustomEvent(this, 'ondraw');
+
+
+
+
+
+
+
+
+
+
+            //
+            // Install any inline responsive configuration. This
+            // should be last in the draw function - even after
+            // the draw events.
+            //
+            RGraph.installInlineResponsive(this);
+
+
+
+
+
+
+
+
             
             return this;
         };
