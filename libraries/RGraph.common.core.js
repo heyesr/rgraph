e255fcff5aa13566dd7f6bb70fe57d80c3d4d374
Richard <richardheyes@gmail.com>
2024-08-14 11:27:37 +0200

    ws

--- libraries/RGraph.common.core.js
+++ libraries/RGraph.common.core.js
@@ -9,6 +9,7 @@
     // | commercial license option available. See the URL above for more details.        |
     // o---------------------------------------------------------------------------------o
 
+
     window.RGraph = window.RGraph || {isrgraph:true,isRGraph: true,rgraph:true};
 
 // Module pattern
